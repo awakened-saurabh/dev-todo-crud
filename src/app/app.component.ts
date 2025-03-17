71e74fcb40f2b573991b3c9793be0c45c3f7e625
Saurabh <saurabhashutosh4@gmail.com>
2025-03-17 11:53:51 +0530

    todo_CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  checked: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule,NgFor,NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  todolist: TodoItem[] = [];
+  newTask: string = '';
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTasktolist: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        checked: false,
+      };
+      this.todolist.push(newTasktolist);
+      console.log(this.todolist);
+      this.newTask = '';
+    }
+  }
+
+  toggleCompleted(index: number): void {
+  this.todolist[index].checked = !this.todolist[index].checked;
+  }
+  deleteTask(id:number): void {
+  this.todolist=this.todolist.filter(item=>item.id!==id);
+  console.log(this.todolist);
+  }
+
+  updateTask(index: number): void {
+    const newTask=prompt('Update Task', this.todolist[index].task);
+    if(newTask!==null){
+      this.todolist[index].task=newTask;
+    }
+  }
 }
